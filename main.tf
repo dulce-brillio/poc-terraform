36b8cb841caf6a935a574581784a00bcc59a776e
dulce-brillio <133916719+dulce-brillio@users.noreply.github.com>
2023-07-12 14:55:23 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider {
+  region = "us-east-2"
+  secret_key = ""
+  access_key = ""
+}
+
+terraform{
+  backend "s3" {
+    encrypt = false
+    bucket = "tf-state1111"
+    dynamodb_table = "tf-state-lock-dynamo"
+    key = "path/path/terraform-tfstate"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
