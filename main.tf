3a19ddf46f7ca894a278301a30e2204c86ea6723
dulce-brillio <133916719+dulce-brillio@users.noreply.github.com>
2023-07-12 16:48:19 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform{
     encrypt = false
     bucket = "tf-state1111"
     dynamodb_table = "tf-state-lock-dynamo"
-    key = "state_fles/"
+    key = "state_files/"
     region = "us-east-2"
   }
 }
